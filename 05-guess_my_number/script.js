ce953b5fe37177ad4db6eae90b052561554e418a
SemukeloMathe <semmathe@gmail.com>
2025-05-21 00:42:40 +0200

    implemented high score logic

--- 05-guess_my_number/script.js
+++ 05-guess_my_number/script.js
@@ -2,9 +2,10 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20
-let highscore;
+let highscore = 0;
 
 document.querySelector(".score").textContent = score;
+document.querySelector(".highscore").textContent = highscore;
 
 document.querySelector(".check").addEventListener("click", e => {
     const guess = Number(document.querySelector(".guess").value);
@@ -22,6 +23,10 @@ document.querySelector(".check").addEventListener("click", e => {
         document.querySelector(".number").textContent = secretNumber;
         document.querySelector("body").style.backgroundColor = "#60b347";
         document.querySelector(".number").style.width = "30rem";
+
+
+        highscore = score > highscore ? score : highscore;
+        document.querySelector(".highscore").textContent = highscore;
 
         // when the guess is too high
     } else if (guess > secretNumber) {
@@ -48,7 +53,6 @@ document.querySelector(".check").addEventListener("click", e => {
         }
     }
 });
-
 
 document.querySelector(".again").addEventListener("click", (e) => {
     secretNumber = Math.trunc(Math.random() * 20) + 1;
