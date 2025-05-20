0eb7b033fea05fb4bd7eb43f05c6a68bc75610d3
SemukeloMathe <semmathe@gmail.com>
2025-05-21 00:24:20 +0200

    play again button logic, resets values to default

--- 05-guess_my_number/script.js
+++ 05-guess_my_number/script.js
@@ -1,23 +1,29 @@
 "use strict";
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20
+let highscore;
 
-document.querySelector(".number").textContent = secretNumber;
 document.querySelector(".score").textContent = score;
 
 document.querySelector(".check").addEventListener("click", e => {
     const guess = Number(document.querySelector(".guess").value);
     console.log(guess, typeof guess);
 
+    // when there's no input
     if (!guess) {
 
         document.querySelector(".message").textContent = "⛔ No number! ⛔";
 
+        // when the player wins 
     } else if (guess === secretNumber) {
 
         document.querySelector(".message").textContent = "🎉 Correct number 🎉";
+        document.querySelector(".number").textContent = secretNumber;
+        document.querySelector("body").style.backgroundColor = "#60b347";
+        document.querySelector(".number").style.width = "30rem";
 
+        // when the guess is too high
     } else if (guess > secretNumber) {
 
         if (score > 1) {
@@ -29,6 +35,7 @@ document.querySelector(".check").addEventListener("click", e => {
             document.querySelector(".score").textContent = 0;
         }
 
+        // when the guess is too low
     } else if (guess < secretNumber) {
 
         if (score > 1) {
@@ -40,4 +47,17 @@ document.querySelector(".check").addEventListener("click", e => {
             document.querySelector(".score").textContent = 0;
         }
     }
+});
+
+
+document.querySelector(".again").addEventListener("click", (e) => {
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    score = 20;
+
+    document.querySelector(".score").textContent = score;
+    document.querySelector(".guess").value = "";
+    document.querySelector(".number").textContent = "?";
+    document.querySelector(".number").style.width = "15rem";
+    document.querySelector("body").style.backgroundColor = "#222";
+    document.querySelector(".message").textContent = "Start guessing...";
 });
