d2b270bc99ee79035857f87e69eef1071ba32381
SemukeloMathe <semmathe@gmail.com>
2025-05-20 23:55:33 +0200

    implemented game logic

--- 05-guess_my_number/script.js
+++ 05-guess_my_number/script.js
@@ -1,12 +1,43 @@
-'use strict';
+"use strict";
 
-document.querySelector(".check").addEventListener(
-    "click", (e) => {
-        const guess = Number(document.querySelector(".guess").value)
-        console.log(guess, typeof guess)
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20
 
-        if (!guess) {
-            document.querySelector(".message").textContent = "⛔ No number!"
+document.querySelector(".number").textContent = secretNumber;
+document.querySelector(".score").textContent = score;
+
+document.querySelector(".check").addEventListener("click", e => {
+    const guess = Number(document.querySelector(".guess").value);
+    console.log(guess, typeof guess);
+
+    if (!guess) {
+
+        document.querySelector(".message").textContent = "⛔ No number! ⛔";
+
+    } else if (guess === secretNumber) {
+
+        document.querySelector(".message").textContent = "🎉 Correct number 🎉";
+
+    } else if (guess > secretNumber) {
+
+        if (score > 1) {
+            document.querySelector(".message").textContent = "📈 Too high!";
+            score--;
+            document.querySelector(".score").textContent = score;
+        } else {
+            document.querySelector(".message").textContent = "💥 Game Over! You lost the game 💥";
+            document.querySelector(".score").textContent = 0;
+        }
+
+    } else if (guess < secretNumber) {
+
+        if (score > 1) {
+            document.querySelector(".message").textContent = "📉 Too low!";
+            score--;
+            document.querySelector(".score").textContent = score;
+        } else {
+            document.querySelector(".message").textContent = "💥 Game Over! You lost the game 💥";
+            document.querySelector(".score").textContent = 0;
         }
     }
-);
+});
