29805086e2761316a349ab6785039253037cbcc4
SemukeloMathe <semmathe@gmail.com>
2025-05-20 22:51:59 +0200

    added event listener to the check button

--- 05-guess_my_number/script.js
+++ 05-guess_my_number/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+document.querySelector(".check").addEventListener(
+    "click", (e) => {
+        const guess = Number(document.querySelector(".guess").value)
+        console.log(guess, typeof guess)
+
+        if (!guess) {
+            document.querySelector(".message").textContent = "⛔ No number!"
+        }
+    }
+);
