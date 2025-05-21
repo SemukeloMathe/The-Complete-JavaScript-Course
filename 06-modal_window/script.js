bf6d1ef3d82ea82e0e312a9e65e03b2d4c487061
SemukeloMathe <semmathe@gmail.com>
2025-05-22 00:03:01 +0200

    added event listener to close modal button

--- 06-modal_window/script.js
+++ 06-modal_window/script.js
@@ -13,4 +13,9 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
         modal.classList.remove("hidden");
         overlay.classList.remove("hidden");
     });
-}
+};
+
+btnCloseModal.addEventListener("click", () => {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+});
