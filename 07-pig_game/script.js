3b57f7c58b0e612f2894d7a6a0edb078f9e5f656
SemukeloMathe <semmathe@gmail.com>
2025-05-22 21:19:10 +0200

    created hidden class in styles and set the scores to 0

--- 07-pig_game/script.js
+++ 07-pig_game/script.js
@@ -1,1 +1,13 @@
 "use strict";
+
+// grabbing elements
+// selecting the scores
+const score0El = document.getElementById("score--0");
+const score1El = document.getElementById("score--1");
+// selecting the dice
+const dicEl = document.querySelector(".dice");
+
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+dicEl.classList.add("hidden");
