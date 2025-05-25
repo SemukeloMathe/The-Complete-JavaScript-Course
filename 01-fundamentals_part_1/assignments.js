458b0f4d87b728dedfca9617cdfb86db3d2aa76c
SemukeloMathe <semmathe@gmail.com>
2025-05-25 08:57:58 +0200

    01 fundamentals and assignment 1

---
+++ 01-fundamentals_part_1/assignments.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/**
+ * Lecture: Values & Variables (Assignment 1)
+ * 1. Declare variables called "country", "continent", and "population" and
+ * assign their values according to your own country (population in the millions)
+ * 2. Log their values to the console.
+ */
+
+let country = "South Africa";
+let continent = "Africa";
+let population = 63.21;
+
+console.log("Country :" + country);
+console.log("Continet: " + continent);
+console.log("Population: " + population + " million");
---
+++ 01-fundamentals_part_1/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let firstName = "John";
+let lastName = "Doe";
+let age = 26;
+
+let $function = () => console.log("im a function");
+$function();
+
+let _function = () => console.log("im also a function");
+_function();
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
