15cc20a1ee1d2e0c9c020612e8df748bed16e0aa
SemukeloMathe <semmathe@gmail.com>
2025-05-27 16:44:17 +0200

    code: coding challenge 4 solution

--- 01-fundamentals_part_1/codingChallenge4.js
+++ 01-fundamentals_part_1/codingChallenge4.js
@@ -11,9 +11,26 @@
  * 2. Print a string to the console containing the bill value, the tip and the
  * final value (bill + tip). Example: "The bill was 275, the tip was 41.25 and
  * the total value 316.25"
- * 
- * Test Data: Test for bill values 275, 40 and 430.
+ *
  * HINT: To calculate 20% of a value, simply multiply it by 20/100 = 0.2
  * HINT: Value X is between 50 and 300, if it's >= 50 && <= 300 😉
-*/
+ *
+ * Test Data: Test for bill values 275, 40 and 430.
+ */
 
+// Test Data 1
+// const bill = 275;
+
+// Test Data 2
+// const bill = 40;
+
+// Test Data 3
+const bill = 430;
+
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+console.log(
+    `The bill was R${bill.toFixed(2)}, the tips was R${tip.toFixed(
+        2
+    )} and the total value R${(bill + tip).toFixed(2)}`
+);
