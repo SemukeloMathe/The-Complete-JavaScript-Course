c5d52aef0704d4db27d7e14e749d2c94c26e6921
SemukeloMathe <semmathe@gmail.com>
2025-05-29 21:16:58 +0200

    code: coding challenge 1

--- 02-fundamentals_part_2/script.js
+++ 02-fundamentals_part_2/script.js
@@ -1,2 +1,26 @@
 "use strict";
 
+// array methods
+// to add elements
+const friends = new Array("Sem", "Legend", "Mathe");
+const pushed = friends.push("Sucre");
+
+// console.log(pushed);
+// console.log(friends);
+
+// friends.unshift("John");
+// console.log(friends);
+
+// to remove elements, these don't take in any args
+// const popped = friends.pop();
+// console.log(popped);
+// console.log(friends);
+
+// const shifted = friends.shift()
+// console.log(shifted);
+// console.log(friends);
+
+// positions of elements
+console.log(friends.indexOf("Legend"));
+
+console.log(friends.includes("Mathe"));
