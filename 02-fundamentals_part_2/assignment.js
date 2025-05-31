3055f1c162282eb9c96ef607956435e571097118
SemukeloMathe <semmathe@gmail.com>
2025-05-31 22:34:16 +0200

    assignment: lecture 1 - functions

--- 02-fundamentals_part_2/assignment.js
+++ 02-fundamentals_part_2/assignment.js
@@ -1,1 +1,13 @@
 "use strict";
+
+/**
+ * Lecture: Functions
+ * 1. Write a function called "describeCountry" which takes three parameters
+ * "country", "population", & "capitalCity". Based on this input, the function
+ * returns a string in this format: "Finland has 6 million people and its 
+ * capital city is Helsinki".
+ * 2. Call the function 3 timed with input data for 3 different countries.
+ * Store the returned valies in 3 different variables and log them to the
+ * console.
+ */
+
