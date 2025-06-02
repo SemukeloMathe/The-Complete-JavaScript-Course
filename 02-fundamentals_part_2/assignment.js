5657f60593750990c3febe2b83462d248d9a7142
SemukeloMathe <semmathe@gmail.com>
2025-06-02 18:34:29 +0200

    assignment 5: starter question

--- 02-fundamentals_part_2/assignment.js
+++ 02-fundamentals_part_2/assignment.js
@@ -102,7 +102,15 @@ console.log(namPopDesc);
 
 /**
  * Lecture: Introduction to Arrays
+ * 1. Create an array containing 4 population values of 4 countries of your
+ * choice. Store this array into a variable called "populations".
+ * 2. Log to the console whether the array has 4 elements or not (true or false)
+ * 3. Create an array called "percentages" containing the percentages of the
+ * world population for these 4 populations. Use the function "percentageOfWorld1"
+ * that you created earlier to compute the 4 percentage values 
  */
+
+
 
 /**
  * Lecture: Basic Array Operations (Methods)
