77075c3cfe7c8f3f3fa0e6e6622aa9a6fb505926
SemukeloMathe <semmathe@gmail.com>
2025-06-03 21:11:00 +0200

    assigbment 10 solution

--- 02-fundamentals_part_2/assignment.js
+++ 02-fundamentals_part_2/assignment.js
@@ -188,11 +188,13 @@ const myCountry = {
 
 /**
  * Lecture: Iteration - the for loop
+ * 1. There are elections in your country! In a small town, there are only 50
+ * voters. Use a for loop to simulate the 50 people voting, by logging a string
+ * to the console(for numbers 1 to 50): "Voter number 1 is currently voting."
  */
 
-/**
- * Lecture: Looping Arrays, Breaking & Continuing
- */
+for (let i = 1; i <= 50; i++)
+    console.log(`Voter number ${i} is currently voting.`);
 
 /**
  * Lecture: Looping Backwards & Nested Loops
