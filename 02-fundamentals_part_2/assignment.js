440bcf898d3843084d991b93f989d3a1a64f3ec4
SemukeloMathe <semmathe@gmail.com>
2025-06-03 21:16:20 +0200

    assignment 9 starter code

--- 02-fundamentals_part_2/assignment.js
+++ 02-fundamentals_part_2/assignment.js
@@ -184,7 +184,17 @@ const myCountry = {
 
 /**
  * Lecture: Object Methods
+ * 1. Add a method called "describe" to the "myCountry" object. This method will
+ * log a string to the console, similar to the string logged in the previous
+ * assignment, but this time using the "this" keyword.
+ * 2. Call the "describe" method
+ * 3. Add a method called "checkIsIsland" to the "myCountry" object. This method
+ * will set a new property on the object, called "isIsland".
+ * "isIsland" will be true if there are no neighbouring countries & false other
+ * -wise. Use the ternary operator to set the property.
  */
+
+
 
 /**
  * Lecture: Iteration - the for loop
