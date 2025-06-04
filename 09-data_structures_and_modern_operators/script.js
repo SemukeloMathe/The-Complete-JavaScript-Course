cf28e5b28d6712b768c2cec6e2f9711324012e17
SemukeloMathe <semmathe@gmail.com>
2025-06-04 21:27:38 +0200

    destructuring arrays

--- 09-data_structures_and_modern_operators/script.js
+++ 09-data_structures_and_modern_operators/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/** 
 // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
@@ -21,14 +21,18 @@ const mexicanFoods = new Set([
   'avocado',
   'garlic',
 ]);
+*/
 
 // Data needed for first part of the section
 const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
 
   openingHours: {
     thu: {
@@ -45,3 +49,25 @@ const restaurant = {
     },
   },
 };
+
+// Array destructuring.
+const arr = [2, 3, 4];
+const [a, b, c] = arr;
+// console.log(a, b, c)
+
+let [main, secondary] = restaurant.categories;
+console.log(main, secondary);
+
+[main, secondary] = new Array(secondary, main);
+console.log(main, secondary);
+
+const [starter, mainMeal] = restaurant.order(2, 0);
+console.log(starter, mainMeal);
+
+const nested = [2, 4, [5, 6]];
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+// Default values in destructuring.
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
