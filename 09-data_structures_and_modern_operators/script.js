70884db95be4d64b6b4fd38f2299280c11aca722
SemukeloMathe <semmathe@gmail.com>
2025-06-05 09:00:11 +0200

    optional chaining

--- 09-data_structures_and_modern_operators/script.js
+++ 09-data_structures_and_modern_operators/script.js
@@ -191,6 +191,7 @@ console.log(add(...x));
 
 restaurant.orderPizza("Mushroom", "Olives", "Onion", "Spinach");
 */
+
 /**
 // Short circuiting / Short-circuit evaluation
 console.log("-------OR--------");
@@ -260,6 +261,7 @@ console.log(rest1);
 console.log(rest2);
 */
 
+/** 
 // The for of loop.
 const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
@@ -271,3 +273,75 @@ for (const [i, el] of menu.entries()) {
 }
 
 // console.log([...menu.entries()])
+*/
+
+// Enhanced Object Literals.
+/**
+// es6 introduced 3 new ways that make it easier to write object literals
+const openingHours = {
+    thu: {
+        open: 12,
+        close: 22,
+    },
+    fri: {
+        open: 11,
+        close: 23,
+    },
+    sat: {
+        open: 0, // Open 24 hours
+        close: 24,
+    },
+}
+
+// no need to write the property name
+const resta = {
+    openingHours,
+}
+// also no need to write the property name for object methods.
+const resta2 = {
+    order(starterIndex, mainIndex) {
+        return starterIndex, mainIndex;
+    } // perfectly valid syntax.
+}
+// we can now compute property names instead of manually writing them.
+const weekdays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]
+
+const rest3 = {
+    [weekdays[3]]: {
+        open: 12,
+        close: 22,
+    },
+    [weekdays[4]]: {
+        open: 11,
+        close: 23,
+    },
+    [weekdays[5]]: {
+        open: 0, // Open 24 hours
+        close: 24,
+    },
+};
+
+console.log(rest3);
+*/
+
+// Optional chaining
+// if a certain property doesn't exist, undefined is returned immediately.
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+console.log(restaurant.openingHours?.fri?.open);
+
+const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+for (const day of days) {
+    const open = restaurant.openingHours[day]?.open ?? "closed";
+    console.log(`On ${day} we open at ${open}`);
+}
+
+// on methods
+console.log(restaurant.order?.(0, 1) ?? "Method does not exist");
+console.log(restaurant.orderRissoto?.(0, 1) ?? "Method does not exist");
+
+// on arrays, used to check if array is empty
+const users = [];
+console.log(users[0]?.name ?? "user array empty");
+
+// Looping objects: object keys, values, entries.
