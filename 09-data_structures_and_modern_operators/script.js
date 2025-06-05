0a676648fb975d239180dfe65f04e8b64cbeafe0
SemukeloMathe <semmathe@gmail.com>
2025-06-05 22:07:19 +0200

    map iteration

--- 09-data_structures_and_modern_operators/script.js
+++ 09-data_structures_and_modern_operators/script.js
@@ -434,9 +434,61 @@ const time = 21;
 // we can also clear all the elements from the map.
 // console.log(restMap.clear());
 // we can also use objects as map keys.
-restMap.set([1, 2], "Test");
-console.log(restMap.get([1, 2])); // results in undefined.
+// restMap.set([1, 2], "Test");
+// console.log(restMap.get([1, 2])); // results in undefined.
 // console.log(restMap);
-const arr = [1, 2];
-restMap.set(arr, "Test");
-console.log(restMap.get(arr));
+// const arr = [1, 2];
+// restMap.set(arr, "Test");
+// console.log(restMap.get(arr));
+
+// creating a new map, use a nested array.
+const question = new Map([
+    ["question", "What is the best programming language in the world?"],
+    [1, "C"],
+    [2, "Java"],
+    [3, "JavaScript"],
+    [4, "Python"],
+    [5, "C++"],
+    ["correct", 3],
+    [true, "correct"],
+    [false, "try again"]
+]);
+// console.log(question);
+
+// convert object to map.
+const openingHours = {
+    thu: {
+        open: 12,
+        close: 22,
+    },
+    fri: {
+        open: 11,
+        close: 23,
+    },
+    sat: {
+        open: 0, // Open 24 hours
+        close: 24,
+    },
+};
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap)
+// iterating through a map.
+// quiz app
+console.log(question.get("question"));
+for (const [key,value] of question) {
+    if (typeof key === "number") {
+        console.log(`Answer ${key}: ${value}`);
+    }
+}
+// const answer = Number(prompt("Your answer: "))
+// console.log(answer);
+const answer = 3;
+
+// console.log(answer === question.get("correct") ? question.get(true) : question.get(false));
+console.log(question.get(answer === question.get("correct")));
+
+// converting a map to an array.
+console.log([...question]);
+console.log([...question.entries()]);
+console.log([...question.values()]);
+console.log([...question.keys()]);
