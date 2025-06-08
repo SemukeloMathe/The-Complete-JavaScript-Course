6328e84ec8c9ea0026145683011e535475fa934a
SemukeloMathe <semmathe@gmail.com>
2025-06-08 19:54:24 +0200

    array methods

--- 11-array_bankist/script.js
+++ 11-array_bankist/script.js
@@ -1,7 +1,5 @@
 'use strict';
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
 // BANKIST APP
 
 // Data
@@ -61,8 +59,6 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
 // LECTURES
 
 const currencies = new Map([
@@ -73,4 +69,35 @@ const currencies = new Map([
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-/////////////////////////////////////////////////
+// let arr = ["a", "b", "c", "d", "e"];
+
+// slice() method.
+// console.log(arr.slice(2));
+// console.log(arr.slice(2, 4));
+// console.log(arr.slice(1, -2));
+// console.log(arr.slice()); // creates a shallow copy.
+
+// splice() method - mutates the original array.
+// console.log(arr.splice(2));
+// console.log(arr);
+// console.log(arr.splice(1, 1));
+// console.log(arr);
+
+// reverse() method - mutates the original array.
+// const arr2 = ["j", "i", "h", "g", "f"];
+// console.log(arr2.reverse());
+// console.log(arr2.slice().reverse());
+// console.log(arr2);
+
+// concat() method - concatenates 2 arrays.
+// const letters = arr.concat(arr2);
+// console.log(letters);
+
+// join() method - returns a string
+// console.log(letters.join(" - "));
+
+// at() method - retrieves an element at a specified index.
+const arr = [23, 11, 64];
+// console.log(arr[arr.length - 1]);
+console.log(arr.at(-1));
+
