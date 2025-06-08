64e18ea8523d36fd9ca7c92b5d05d66564908104
SemukeloMathe <semmathe@gmail.com>
2025-06-08 14:46:56 +0200

    closures examples

--- 10-functions/script.js
+++ 10-functions/script.js
@@ -198,8 +198,47 @@ const secureBooking = function () {
     };
 };
 
-const booker = secureBooking();
-booker();
-booker();
-booker();
-console.dir(booker);
+// const booker = secureBooking();
+// booker();
+// booker();
+// booker();
+// console.dir(booker);
+
+let f;
+
+const g = function () {
+    const a = 23;
+    f = function () {
+        console.log(a * 2);
+    };
+};
+
+const h = function () {
+    const b = 777;
+    f = function () {
+        console.log(b * 2);
+    };
+}
+
+// g();
+// f();
+// console.dir(f);
+// h();
+// f();
+// console.dir(f);
+
+// example 2
+const boardPassengers = function (n, wait) {
+    const perGroup = n / 3;
+
+    setTimeout(function () {
+        console.log(`We are now boarding all ${n} passengers.`);
+        console.log(`There are 3 groups each with ${perGroup} passengers.`);
+    }, wait * 1000);
+
+    console.log(`Will start boarding in ${wait} seconds.`);
+};
+
+const perGroup = 120;
+boardPassengers(180, 3);
+// 
