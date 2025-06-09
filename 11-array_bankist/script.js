b00414391ba444aa8b2bd537d1047f9802c7af07
SemukeloMathe <semmathe@gmail.com>
2025-06-10 00:16:34 +0200

    commented out code on chaining methods lecture

--- 11-array_bankist/script.js
+++ 11-array_bankist/script.js
@@ -199,8 +199,8 @@ calcDisplayBalance(account1.movements);
 
 // Map, filter & reduce
 // 1. map() method - returns a new array
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-const eurToUsd = 1.1;
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const eurToUsd = 1.1;
 
 // const usdMov = movements.map((mov) => mov * eurToUsd);
 // console.log(usdMov);
@@ -244,11 +244,11 @@ const eurToUsd = 1.1;
 // console.log(minValue);
 
 // chaining methods.
-const totalUsdDeposit = Number(
-    movements
-        .filter((mov) => mov > 0)
-        .map((mov) => mov * eurToUsd)
-        .reduce((acc, mov) => acc + mov, 0)
-);
+// const totalUsdDeposit = Number(
+//     movements
+//         .filter((mov) => mov > 0)
+//         .map((mov) => mov * eurToUsd)
+//         .reduce((acc, mov) => acc + mov, 0)
+// );
 
-console.log(totalUsdDeposit);
+// console.log(totalUsdDeposit);
