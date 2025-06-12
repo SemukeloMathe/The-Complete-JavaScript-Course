7ccced8b8e52b6875961b9850cb5958b07a4a53e
SemukeloMathe <semmathe@gmail.com>
2025-06-13 00:01:26 +0200

    fix: daysPassed bug

--- 12-numbers_dates_timers_bankist/script.js
+++ 12-numbers_dates_timers_bankist/script.js
@@ -83,7 +83,8 @@ const formatMovementDate = function (date) {
     const calcDaysPassed = (date1, date2) => {
         return Math.round(Math.abs((date2 - date1) / (1000 * 60 * 60 * 24)));
     };
-    const daysPassed = calcDaysPassed((new Date(), date));
+    const daysPassed = calcDaysPassed(new Date(), date);
+ 
     if (daysPassed === 0) return "Today";
     if (daysPassed === 1) return "Yesterday";
     if (daysPassed <= 7) return `${daysPassed} days ago`;
