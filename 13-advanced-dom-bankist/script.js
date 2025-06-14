ae350b0811956c69697340f0e0185320a4f3a836
SemukeloMathe <semmathe@gmail.com>
2025-06-14 23:33:05 +0200

    change openModal to arrow function

--- 13-advanced-dom-bankist/script.js
+++ 13-advanced-dom-bankist/script.js
@@ -18,11 +18,7 @@ const closeModal = function () {
     modal.classList.add("hidden");
     overlay.classList.add("hidden");
 };
-btnsOpenModal.forEach(function (btn) {
-    btn.addEventListener("click", openModal);
-});
-// for (let i = 0; i < btnsOpenModal.length; i++)
-    // btnsOpenModal[i].addEventListener("click", openModal);
+btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
 
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
