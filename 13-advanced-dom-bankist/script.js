a5f1d22451b6a8908ef5a1d031981434a7cec2f4
SemukeloMathe <semmathe@gmail.com>
2025-06-15 22:37:47 +0200

    creating elements

--- 13-advanced-dom-bankist/script.js
+++ 13-advanced-dom-bankist/script.js
@@ -39,13 +39,28 @@ document.addEventListener("keydown", function (e) {
 console.log(document.documentElement);
 console.log(document.head);
 console.log(document.body);
-document.querySelector(".header");
+const header = document.querySelector(".header");
 const allSections = document.querySelectorAll(".section");
 console.log(allSections);
 
-document.getElementById("section--1")
-const allButtons = document.getElementsByTagName("button")
+document.getElementById("section--1");
+const allButtons = document.getElementsByTagName("button");
 console.log(allButtons);
 
 const btns = document.getElementsByClassName("btn");
-console.log(btns)
+console.log(btns);
+
+// Creating & inseting elements.
+// .insertAdjacentHTML
+const message = document.createElement("div");
+message.classList.add("cookie-message");
+message.textContent = "We use cookies for improved functionality & analytics.";
+message.innerHTML =
+    "<p>We use cookies for improved functionality & analytics.</p><button class=btn btn--close-cookie>Got it!</button>";
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true));
+// header.before(message);
+// header.after(message);
+
+// Delete elements
