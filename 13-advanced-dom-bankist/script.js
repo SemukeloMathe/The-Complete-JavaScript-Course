83ca551ebbbc1da1c1f01c7b49bc2c42b741281d
SemukeloMathe <semmathe@gmail.com>
2025-06-16 22:40:07 +0200

    handling more events

--- 13-advanced-dom-bankist/script.js
+++ 13-advanced-dom-bankist/script.js
@@ -112,7 +112,7 @@ const btnScrollTo = document.querySelector(".btn--scroll-to");
 const section1 = document.querySelector("#section--1");
 
 btnScrollTo.addEventListener("click", (e) => {
-    const s1coords = section1.getBoundingClientRect()
+    const s1coords = section1.getBoundingClientRect();
     console.log(s1coords);
 
     // console.log(e.target.getBoundingClientRect())
@@ -130,4 +130,15 @@ btnScrollTo.addEventListener("click", (e) => {
 
     // modern way
     section1.scrollIntoView({ behavior: "smooth" });
-})
+});
+
+// different ways of handling events.
+const h1 = document.querySelector("h1");
+
+const alertH1 = function (e) {
+    alert("addEventListener: Great! You are reading the heading :D");
+};
+
+h1.addEventListener("mouseenter", alertH1);
+
+setTimeout(() => h1.removeEventListener("mouseenter", h1), 5000);
