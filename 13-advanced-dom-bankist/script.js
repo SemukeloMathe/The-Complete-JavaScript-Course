7102f1b28f0a954bd62437e22fadc7f8095f5120
SemukeloMathe <semmathe@gmail.com>
2025-06-17 19:09:28 +0200

    tabbed component

--- 13-advanced-dom-bankist/script.js
+++ 13-advanced-dom-bankist/script.js
@@ -58,6 +58,30 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
     }
 });
 
+// Tabbed component.
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+
+tabsContainer.addEventListener("click", function (e) {
+    // Matching strategy
+    const clicked = e.target.closest(".operations__tab");
+
+    // ignore clicks where it returns null, Guard clause.
+    if (!clicked) return;
+    // active tabs
+    tabs.forEach(t => t.classList.remove("operations__tab--active"));
+    clicked.classList.add("operations__tab--active");
+
+    // display content for active tab
+    tabsContent.forEach((c) =>
+        c.classList.remove("operations__content--active")
+    );
+    document.querySelector(`.operations__content--${clicked.dataset.tab}`)
+        .classList.add("operations__content--active");
+});
+// tabs.forEach(t => t.addEventListener("click", () => console.log("TAB")));
+
 ///////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////
@@ -178,31 +202,31 @@ document.documentElement.style.setProperty("--color-primary", "orangered");
 const h1 = document.querySelector("h1");
 
 // going downwards.
-console.log(h1.querySelectorAll(".highlight"));
+// console.log(h1.querySelectorAll(".highlight"));
 // console.log(h1.childNodes);
 // console.log(h1.children);
 // console.log(h1.firstElementChild);
 // console.log(h1.lastElementChild);
 
-h1.firstElementChild.style.color = "white";
-h1.lastElementChild.style.color = "white";
+// h1.firstElementChild.style.color = "white";
+// h1.lastElementChild.style.color = "white";
 
 // going upwards
-console.log(h1.parentNode);
-console.log(h1.parentElement);
+// console.log(h1.parentNode);
+// console.log(h1.parentElement);
 
-h1.closest(".header").style.background = "var(--gradient-secondary)";
-h1.closest("h1").style.background = "var(--gradient-primary)";
+// h1.closest(".header").style.background = "var(--gradient-secondary)";
+// h1.closest("h1").style.background = "var(--gradient-primary)";
 
 // going sideways: siblings
-console.log(h1.previousElementSibling);
-console.log(h1.nextElementSibling);
+// console.log(h1.previousElementSibling);
+// console.log(h1.nextElementSibling);
 
-console.log(h1.previousSibling);
-console.log(h1.nextSibling);
+// console.log(h1.previousSibling);
+// console.log(h1.nextSibling);
 
-console.log(h1.parentElement.children);
+// console.log(h1.parentElement.children);
 
-[...h1.parentElement.children].forEach(function (el) {
-    if (el !== h1) el.style.transform = "scale(0.5)";
-});
+// [...h1.parentElement.children].forEach(function (el) {
+//     if (el !== h1) el.style.transform = "scale(0.5)";
+// });
