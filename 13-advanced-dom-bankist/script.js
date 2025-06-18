1ebc490ec5303a15167e7bdad05174afb1c47119
SemukeloMathe <semmathe@gmail.com>
2025-06-18 21:26:15 +0200

    reveal sections as you scroll using observer api

--- 13-advanced-dom-bankist/script.js
+++ 13-advanced-dom-bankist/script.js
@@ -138,6 +138,24 @@ const headerObserver = new IntersectionObserver(
 );
 headerObserver.observe(header)
 
+
+// Reveal sections as you scroll.
+const allSections = document.querySelectorAll(".section");
+const revealSection = function (entries, observer) {
+    const [entry] = entries;
+    console.log(entry);
+    if (!entry.isIntersecting) return;
+    entry.target.classList.remove("section--hidden");
+    observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {root:null, threshold:0.15});
+allSections.forEach(function (section) {
+    sectionObserver.observe(section);
+    section.classList.add("section--hidden");
+})
+
+
 ///////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////
