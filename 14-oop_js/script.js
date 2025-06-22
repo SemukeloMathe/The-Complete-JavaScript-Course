f4f799164a481111b0a554584c123e9f46a35b0d
SemukeloMathe <semmathe@gmail.com>
2025-06-22 17:50:56 +0200

    using the function constructor to create new objects

--- 14-oop_js/script.js
+++ 14-oop_js/script.js
@@ -1,1 +1,32 @@
 "use strict";
+
+// using constructor functions to create objects.
+const Person = function (name, year) {
+    this.name = name;
+    this.year = year;
+
+    this.calcAge = function () {
+        console.log(new Date().getFullYear() - this.year);
+    }
+}
+
+// call the constructor with the "new" operator
+const sem = new Person("Sem", 1998);
+// console.log(sem.calcAge());
+sem.calcAge()
+
+// Using new
+// 1.New {} is created.
+// 2. function is called, this = {}
+// 3. Nesly created object is then linked to a prototype.
+// 4. function automatically returns the empty object.
+
+const matilda = new Person("Matilda", 2017);
+const jack = new Person("Jack", 2003);
+const susan = new Person("Susan", 1988);
+
+const jay = { name: "Jay", year: 2000 }
+// console.log(jay);
+// console.log(sem instanceof Person);
+// console.log(jay instanceof Person)
+
