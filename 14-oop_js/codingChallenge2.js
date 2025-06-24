75cbe49bdaea02311b740ac17a08dc26a4c11938
SemukeloMathe <semmathe@gmail.com>
2025-06-24 14:03:07 +0200

    coding challenge 2 task 1 - recreate coding challenge 1 using es6 class

---
+++ 14-oop_js/codingChallenge2.js
@@ -0,0 +1,38 @@
+/**
+ * 1. Re-create challenge 1, but this time using an ES6 class.
+ * 2. Add a getter called "speedUS" which returns the current speed in m/h
+ * (divide by 1.6).
+ * 3. Add a setter called "speedUS" which sets the current speed in m/h (but
+ * converts it to km/h before storing the value, by multiplying the input by
+ * 1.6).
+ * 4. Create a new car and experiment with accelerate and brake methods, and
+ * with the getter and setter.
+ *
+ * CAR DATA 1: "Ford" going at 120 km/h
+ */
+
+class Car {
+    constructor(make, speed) {
+        this.make = make;
+        this.speed = speed;
+    }
+
+    accelerate() {
+        this.speed += 10;
+        console.log(`${this.make} is going ${this.speed} km/h`);
+    }
+
+    brake() {
+        this.speed -= 5;
+        console.log(`${this.make} is going ${this.speed} km/h`);
+    }
+
+    get speedUS() {}
+
+    set speedUS(speed) {}
+}
+
+const ford = new Car("Ford", 120);
+console.log(ford);
+ford.accelerate();
+ford.brake();
