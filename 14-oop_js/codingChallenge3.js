3833b714e5fc7be15b800466975d8a5221f0ee1c
SemukeloMathe <semmathe@gmail.com>
2025-06-24 15:08:50 +0200

    coding challenge 3 solution

--- 14-oop_js/codingChallenge3.js
+++ 14-oop_js/codingChallenge3.js
@@ -13,3 +13,62 @@
  *
  * CAR DATA 1: "Tesla" going at 120 km/h, with a charge of 23%
  */
+
+const Car = function (make, speed) {
+    this.make = make;
+    this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+    this.speed += 10;
+    console.log(`"${this.make}" going at ${this.speed} km/h`);
+};
+
+Car.prototype.brake = function () {
+    this.speed -= 5;
+    console.log(`"${this.make}" going at ${this.speed} km/h`);
+};
+
+// 1.
+const EV = function (make, speed, charge) {
+    Car.call(this, make, speed);
+    this.charge = charge;
+};
+
+// link the EV class to the Car class
+EV.prototype = Object.create(Car.prototype);
+
+// 2.
+EV.prototype.chargeBattery = function (chargeTo) {
+    this.charge = chargeTo;
+};
+
+// 3.
+EV.prototype.accelerate = function () {
+    this.speed += 20;
+    this.charge--;
+    console.log(
+        `${this.make} going at ${this.speed} km/h, with a charge of ${this.charge}%`
+    );
+};
+
+EV.prototype.constructor = EV;
+
+// 4.
+const tesla = new EV("Tesla", 120, 23);
+console.log(tesla);
+tesla.chargeBattery(90);
+console.log();
+
+console.log(tesla);
+tesla.accelerate();
+tesla.accelerate();
+tesla.accelerate();
+tesla.accelerate();
+console.log();
+
+tesla.brake();
+console.log(tesla);
+console.log();
+
+console.log(EV.prototype.constructor);
