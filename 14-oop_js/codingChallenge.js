c36af86f5486a83fc8d21cdffae3adef9a5dca24
SemukeloMathe <semmathe@gmail.com>
2025-06-24 12:54:17 +0200

    created instance of Car

--- 14-oop_js/codingChallenge.js
+++ 14-oop_js/codingChallenge.js
@@ -17,9 +17,11 @@ const Car = function (make, speed) {
     this.make = make;
     this.speed = speed;
 };
+const bmw = new Car("BMW", 120);
 
 // 2.
 Car.prototype.accelerate = function () {
-    this.speed += 10
+    this.speed += 10;
     console.log(`"${this.make}" going at ${this.speed}km/h`);
-}
+};
+bmw.accelerate();
