c19df8e43af5c9e85444585f8cbe2d78c193c9eb
SemukeloMathe <semmathe@gmail.com>
2025-07-07 21:49:24 +0200

    refactor: form functionality

--- 15-mapty/script.js
+++ 15-mapty/script.js
@@ -30,38 +30,6 @@ const inputElevation = document.querySelector(".form__input--elevation");
 // takes in 2 callback functions. the first is the success callback and the
 // second is the error callback for handling errors
 
-form.addEventListener("submit", function (e) {
-    e.preventDefault();
-
-    // clear inputfields.
-    inputDistance.value = inputDuration.value = inputElevation.value = "";
-    // display the marker
-    console.log(mapEvent);
-    const { lat, lng } = mapEvent.latlng;
-    console.log(lat, lng);
-
-    L.marker([lat, lng])
-        .addTo(map)
-        .bindPopup(
-            L.popup({
-                // pop up options
-                maxWidth: 250,
-                minWidth: 100,
-                autoClose: false,
-                closeOnClick: false,
-                className: "running-popup",
-            })
-        )
-        .setPopupContent("Workout")
-        .openPopup();
-});
-
-
-inputType.addEventListener("change", function (e) {
-    inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
-    inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
-});
-
 class App {
     #map;
     #mapEvent;
@@ -69,6 +37,17 @@ class App {
     constructor() {
         // call the getPosition function.
         this._getPosition();
+
+        form.addEventListener("submit", this._newWorkout.bind(this));
+
+        inputType.addEventListener("change", function (e) {
+            inputElevation
+                .closest(".form__row")
+                .classList.toggle("form__row--hidden");
+            inputCadence
+                .closest(".form__row")
+                .classList.toggle("form__row--hidden");
+        });
     }
 
     _getPosition() {
@@ -94,18 +73,42 @@ class App {
         }).addTo(this.#map);
 
         // get markers from the map.
-        this.#map.on("click", function (mapE) {
-            this.#mapEvent = mapE;
-            form.classList.remove("hidden");
-            inputDistance.focus();
-        });
+        this.#map.on("click", this._showForm.bind(this));
     }
 
-    _showForm() {}
+    _showForm(mapE) {
+        this.#mapEvent = mapE;
+        form.classList.remove("hidden");
+        inputDistance.focus();
+    }
 
     _toggleElevationField() {}
 
-    _newWorkout() {}
+    _newWorkout(e) {
+        e.preventDefault();
+        // clear inputfields.
+        inputDistance.value = inputDuration.value = inputElevation.value = "";
+
+        // display the marker
+        const { lat, lng } = this.#mapEvent.latlng;
+        console.log(lat, lng);
+
+        L.marker([lat, lng])
+            .addTo(this.#map)
+            .bindPopup(
+                L.popup({
+                    // pop up options
+                    maxWidth: 250,
+                    minWidth: 100,
+                    autoClose: false,
+                    closeOnClick: false,
+                    className: "running-popup",
+                })
+            )
+            .setPopupContent("Workout")
+            .openPopup();
+    }
 }
+
 
 const app = new App();
