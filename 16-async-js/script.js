c6f6bb57c9b7079d0c0acdeb126811d679a9d03e
SemukeloMathe <semmathe@gmail.com>
2025-07-15 21:46:52 +0200

    transform geolocation api to a promise

--- 16-async-js/script.js
+++ 16-async-js/script.js
@@ -201,33 +201,53 @@ const renderCountry = function (data = {}, className = "") {
 // console.log("Test end");
 
 // building a simple promise
-const lotteryPromise = new Promise(function (resolve, reject) {
-    console.log("Lottery draw is happening.");
-    setTimeout(function () {
-      if (Math.random() >= 0.5) {
-        resolve("You Win 💰");
-      } else {
-        reject(new Error("You Lost 🥲"));
-      }
-    }, 2000);
-});
+// const lotteryPromise = new Promise(function (resolve, reject) {
+//     console.log("Lottery draw is happening.");
+//     setTimeout(function () {
+//       if (Math.random() >= 0.5) {
+//         resolve("You Win 💰");
+//       } else {
+//         reject(new Error("You Lost 🥲"));
+//       }
+//     }, 2000);
+// });
 
-lotteryPromise.then((res) => console.log(res))
-    .catch((err) => console.error(err));
+// lotteryPromise.then((res) => console.log(res))
+//     .catch((err) => console.error(err));
 
-const wait = function (seconds) {
-  return new Promise(function (resolve) {
-    setTimeout(resolve, seconds * 1000);
+// const wait = function (seconds) {
+//   return new Promise(function (resolve) {
+//     setTimeout(resolve, seconds * 1000);
+//   });
+// };
+
+// wait(2)
+//   .then(() => {
+//     console.log("I waited for 2 seconds");
+//     return wait(1);
+//   })
+//   .then(() => console.log("I waited for one second"));
+
+// // easy way to create a fulfilled promise
+// Promise.resolve("abc").then(x => console.log(x));
+// Promise.reject(new Error("abc")).then(x => console.error(x));
+
+// Promisifying the geolocation api
+navigator.geolocation.getCurrentPosition(
+  position => console.log(position),
+  err => console.error(err)
+)
+
+console.log("Getting position");
+
+const getPosition = function () {
+  return new Promise(function (resolve, reject) {
+    // navigator.geolocation.getCurrentPosition(
+    //   (position) => resolve(position),
+    //   (err) => reject(err)
+    // );
+    navigator.geolocation.getCurrentPosition(resolve, reject)
   });
-};
+}
 
-wait(2)
-  .then(() => {
-    console.log("I waited for 2 seconds");
-    return wait(1);
-  })
-  .then(() => console.log("I waited for one second"));
-
-// easy way to create a fulfilled promise
-Promise.resolve("abc").then(x => console.log(x));
-Promise.reject(new Error("abc")).then(x => console.error(x));
+getPosition().then(pos => console.log(pos)).catch();
