a2143ac4baa10306c7b17cb5aed23543e1a0fa26
SemukeloMathe <semmathe@gmail.com>
2025-08-01 15:53:20 +0200

    create "showRecipe" function, fetch recipe data from api

--- 18-forkify/src/js/controller.js
+++ 18-forkify/src/js/controller.js
@@ -12,3 +12,18 @@ const timeout = function (s) {
 // https://forkify-api.jonas.io
 
 ///////////////////////////////////////
+const showRecipe = async function (params) {
+    try {
+        const res = await fetch(
+            "https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886"
+        );
+        const data = await res.json();
+
+        if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+        console.log(res, data);
+    } catch (err) {
+        alert(err);
+    }
+};
+showRecipe();
