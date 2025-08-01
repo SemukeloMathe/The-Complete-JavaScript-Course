3e0b4fe38580448acd79b4ef6a2246ee13116a8b
SemukeloMathe <semmathe@gmail.com>
2025-08-01 16:02:25 +0200

    loading recipe from api

--- 18-forkify/src/js/controller.js
+++ 18-forkify/src/js/controller.js
@@ -15,8 +15,10 @@ const timeout = function (s) {
 const showRecipe = async function (params) {
     try {
         const res = await fetch(
-            "https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886"
+            "https://forkify-api.jonas.io/api/v2/recipes/664c8f193e7aa067e94e8658"
+            // "https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886"
         );
+
         const data = await res.json();
 
         if (!res.ok) throw new Error(`${data.message} (${res.status})`);
@@ -36,7 +38,6 @@ const showRecipe = async function (params) {
         };
 
         console.log(recipe);
-
     } catch (err) {
         alert(err);
     }
